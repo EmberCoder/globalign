5143c0bce8a64eddd09e60d513448647e7b50850
Varsha Amirthraj <varshaamirthraj@Varshas-Laptop.attlocal.net>
2025-01-19 11:21:51 -0600

    do it pls

--- addingtime.js
+++ addingtime.js
@@ -1,7 +1,22 @@
 
+
 async function fetchData() {
     try {
-      const response = await fetch('https://api.example.com/data'); 
+      const response = await fetch('https://timeapi.io/api/conversion/converttimezone', {
+        method: "POST",
+        body: JSON.stringify(
+          {
+            "fromTimeZone": continentandcountry,
+            "dateTime": "2021-03-14 17:45:00",
+            "toTimeZone": "America/Los_Angeles",
+            "dstAmbiguity": ""
+          }
+        ),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      }
+      ); 
   
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
